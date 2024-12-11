c57cb922114a885d97165d670af5ab1ba797b1f6
Paola Caballero <paolisco2525@gmail.com>
2024-12-11 11:25:02 -0500

    Laboratio Modulos

---
+++ task2.js
@@ -0,0 +1,14 @@
+export class FriendNames {
+
+    constructor(name1, name2, name3) {
+
+        this.name1 = name1;
+        this.name2 = name2;
+        this.name3 = name3;
+    }
+}
+
+const friendNames = new FriendNames('Karim', 'Uli', 'Georgina');
+
+
+console.log(friendNames);
